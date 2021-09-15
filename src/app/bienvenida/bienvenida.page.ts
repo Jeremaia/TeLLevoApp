9f02d9b63a756c9fb3808a78c6419b77f665a51a
soqus <daniel.caro440@gmail.com>
2021-09-15 02:35:25 -0300

    Se agrega imagen inicial. Animacion sin exito.

--- src/app/bienvenida/bienvenida.page.ts
+++ src/app/bienvenida/bienvenida.page.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-bienvenida',
   templateUrl: './bienvenida.page.html',
   styleUrls: ['./bienvenida.page.scss'],
 })
-export class BienvenidaPage implements OnInit {
+export class BienvenidaPage {
 
-  constructor() { }
+  constructor(private router:Router) {}
 
-  ngOnInit() {
+  cerrar(){
+this.router.navigate(['/ingreso-usuario'])
+
   }
+
 
 }
